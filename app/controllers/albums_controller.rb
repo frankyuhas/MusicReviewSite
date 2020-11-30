efcea205258b721c4b1d341bc8adf6b1e1eb4c25
Sean Fuller <tuj50935@temple.edu>
2020-11-29 21:14:09 -0500

    Big Commit

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -9,10 +9,12 @@ class AlbumsController < ApplicationController
 
 	def new
 		@album = current_user.albums.build
+		@categories = Category.all.map{ |c| [c.name, c.id] }
 	end
 
 	def create
 		@album = current_user.albums.build(album_params)
+		@album.category_id = params[:category_id]
 
 		if @album.save 
 			redirect_to root_path
@@ -22,9 +24,11 @@ class AlbumsController < ApplicationController
 	end
 
 	def edit
+		@categories = Category.all.map{ |c| [c.name, c.id] }
 	end
 
 	def update
+		@album.category_id = params[:category_id]
 		if @album.update(album_params)
 			redirect_to album_path(@album)
 		else
@@ -34,14 +38,14 @@ class AlbumsController < ApplicationController
 
 	def destroy
 		@album.destroy
-		redirect root_path
+		redirect_to root_path
 	end
 
 
 	private
 
 		def album_params
-			params.require(:album).permit(:title, :artist, :songs)
+			params.require(:album).permit(:title, :artist, :songs, :category_id)
 		end
 
 		def find_album
